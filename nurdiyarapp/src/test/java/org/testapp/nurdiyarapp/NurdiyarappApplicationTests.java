a17ac5d1c5205649edbdf7ac2ed37413d146092a
Nurdiyar <nurdi.instagramm@gmail.com>
2025-09-22 21:13:13 +0500

    init spring app

---
+++ nurdiyarapp/src/test/java/org/testapp/nurdiyarapp/NurdiyarappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.testapp.nurdiyarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurdiyarappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
